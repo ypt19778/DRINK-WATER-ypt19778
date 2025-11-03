0851f10ab9391870dc215ada0dd0c2871a8df27f
ypt19778 <littlebigleimicrosft@gmail.com>
2025-11-03 16:21:02 -0500

    + add gameover restart

--- game_over.lua
+++ game_over.lua
@@ -4,6 +4,9 @@ function M:execute(msg, font, x, y, scale)
          love.graphics.setBackgroundColor(0, 0, 0)
          love.graphics.setColor(1, 0, 0)
          love.graphics.print(msg, font, x, y, nil, scale)
+         love.graphics.print('press "r" to restart', 400, 400, nil, 2)
          love.graphics.setColor(1, 1, 1)
+
+         if love.keyboard.isDown('r') then love.event.quit('restart') end
 end
 return M
